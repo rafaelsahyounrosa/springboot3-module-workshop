961bf24ed3e00942ffaf7c2df5de69d5c4210f90
Rafael Rosa <rafaelsahyoun@gmail.com>
2023-01-22 22:05:41 -0300

    project created

---
+++ src/test/java/com/rafaelrosa/springmodule/SpringmoduleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rafaelrosa.springmodule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringmoduleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
