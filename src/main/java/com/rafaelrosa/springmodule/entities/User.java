332dabbab99948f54f07d560bd1c33c0b6a80b5a
Rafael Rosa <rafaelsahyoun@gmail.com>
2023-01-23 11:34:41 -0300

    H2 database, test-profile and JPA

--- src/main/java/com/rafaelrosa/springmodule/entities/User.java
+++ src/main/java/com/rafaelrosa/springmodule/entities/User.java
@@ -1,10 +1,16 @@
 package com.rafaelrosa.springmodule.entities;
 
+import jakarta.persistence.*;
+
 import java.io.Serializable;
 import java.util.Objects;
 
+@Entity
+@Table(name = "tb_user")
 public class User implements Serializable {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String email;
