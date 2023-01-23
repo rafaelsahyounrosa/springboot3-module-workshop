0d1edd6a9bfe6cb84b768aa2abaa90e3df3ba213
Rafael Rosa <rafaelsahyoun@gmail.com>
2023-01-23 19:33:04 -0300

    Service layer, component registration

--- src/main/java/com/rafaelrosa/springmodule/repositories/UserRepository.java
+++ src/main/java/com/rafaelrosa/springmodule/repositories/UserRepository.java
@@ -2,7 +2,9 @@ package com.rafaelrosa.springmodule.repositories;
 
 import com.rafaelrosa.springmodule.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
 
