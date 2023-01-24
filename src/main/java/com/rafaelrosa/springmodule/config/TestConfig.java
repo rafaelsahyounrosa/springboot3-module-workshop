c400ee4be70c0cdb101e09ab3f33a061b52cf5e4
Rafael Rosa <rafaelsahyoun@gmail.com>
2023-01-24 16:18:25 -0300

    Category structure

--- src/main/java/com/rafaelrosa/springmodule/config/TestConfig.java
+++ src/main/java/com/rafaelrosa/springmodule/config/TestConfig.java
@@ -1,8 +1,10 @@
 package com.rafaelrosa.springmodule.config;
 
+import com.rafaelrosa.springmodule.entities.Category;
 import com.rafaelrosa.springmodule.entities.Order;
 import com.rafaelrosa.springmodule.entities.User;
 import com.rafaelrosa.springmodule.entities.enums.OrderStatus;
+import com.rafaelrosa.springmodule.repositories.CategoryRepository;
 import com.rafaelrosa.springmodule.repositories.OrderRepository;
 import com.rafaelrosa.springmodule.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,9 +25,18 @@ public class TestConfig implements CommandLineRunner {
     @Autowired
     private OrderRepository orderRepository;
 
+    @Autowired
+    private CategoryRepository categoryRepository;
+
 
     @Override
     public void run(String... args) throws Exception {
+        Category cat1 = new Category(null, "Electronics");
+        Category cat2 = new Category(null, "Books");
+        Category cat3 = new Category(null, "Computers");
+
+        categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
+
         User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
         User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
 
@@ -36,5 +47,6 @@ public class TestConfig implements CommandLineRunner {
         Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), OrderStatus.WAITING_PAYMENT, u1);
 
         orderRepository.saveAll(Arrays.asList(o1, o2, o3));
+
     }
 }
---
+++ src/main/java/com/rafaelrosa/springmodule/services/CategoryService.java
@@ -0,0 +1,25 @@
+package com.rafaelrosa.springmodule.services;
+
+import com.rafaelrosa.springmodule.entities.Category;
+import com.rafaelrosa.springmodule.repositories.CategoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class CategoryService {
+
+    @Autowired
+    private CategoryRepository repository;
+
+    public List<Category> findAll(){
+        return repository.findAll();
+    }
+
+    public Category findById(Long id){
+        Optional<Category> obj = repository.findById(id);
+        return obj.get();
+    }
+}
