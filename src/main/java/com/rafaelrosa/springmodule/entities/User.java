cef4821b4930f7f732726bc8d5c2c2d3b290099d
Rafael Rosa <rafaelsahyoun@gmail.com>
2023-01-23 21:35:10 -0300

    Order, instant and ISO 8601 part 1

--- src/main/java/com/rafaelrosa/springmodule/entities/User.java
+++ src/main/java/com/rafaelrosa/springmodule/entities/User.java
@@ -3,6 +3,8 @@ package com.rafaelrosa.springmodule.entities;
 import jakarta.persistence.*;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 @Entity
@@ -17,6 +19,9 @@ public class User implements Serializable {
     private String phone;
     private String password;
 
+    @OneToMany(mappedBy = "client")
+    private List<Order> orders = new ArrayList<>();
+
     public User() {
     }
 
@@ -26,6 +31,10 @@ public class User implements Serializable {
         this.email = email;
         this.phone = phone;
         this.password = password;
+    }
+
+    public List<Order> getOrders() {
+        return orders;
     }
 
     public Long getId() {
