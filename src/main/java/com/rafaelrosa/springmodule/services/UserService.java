c19d15072053c5e8fa79d8eab0f441197420d9fb
Rafael Sahyoun Rosa <83467259+rafaelsahyounrosa@users.noreply.github.com>
2023-01-25 12:01:58 -0300

    Exception handler - delete

--- src/main/java/com/rafaelrosa/springmodule/services/UserService.java
+++ src/main/java/com/rafaelrosa/springmodule/services/UserService.java
@@ -2,8 +2,11 @@ package com.rafaelrosa.springmodule.services;
 
 import com.rafaelrosa.springmodule.entities.User;
 import com.rafaelrosa.springmodule.repositories.UserRepository;
+import com.rafaelrosa.springmodule.services.exceptions.DatabaseException;
 import com.rafaelrosa.springmodule.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -29,7 +32,13 @@ public class UserService {
     }
 
     public void delete(Long id){
-        repository.deleteById(id);
+        try {
+            repository.deleteById(id);
+        }catch (EmptyResultDataAccessException e){
+            throw new ResourceNotFoundException(id);
+        }catch (DataIntegrityViolationException e){
+            throw new DatabaseException(e.getMessage());
+        }
     }
 
     public User update(Long id, User obj){
