63fb0401e0332774d693e3edb8581742bfa8108a
Rafael Sahyoun Rosa <83467259+rafaelsahyounrosa@users.noreply.github.com>
2023-01-25 11:48:32 -0300

    Exception handler  - find by id

--- src/main/java/com/rafaelrosa/springmodule/services/UserService.java
+++ src/main/java/com/rafaelrosa/springmodule/services/UserService.java
@@ -2,6 +2,7 @@ package com.rafaelrosa.springmodule.services;
 
 import com.rafaelrosa.springmodule.entities.User;
 import com.rafaelrosa.springmodule.repositories.UserRepository;
+import com.rafaelrosa.springmodule.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -20,7 +21,7 @@ public class UserService {
 
     public User findById(Long id){
         Optional<User> obj = repository.findById(id);
-        return obj.get();
+        return obj.orElseThrow(() -> new ResourceNotFoundException(id));
     }
 
     public User insert(User obj){
---
+++ src/main/java/com/rafaelrosa/springmodule/services/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,8 @@
+package com.rafaelrosa.springmodule.services.exceptions;
+
+public class ResourceNotFoundException extends RuntimeException{
+
+    public ResourceNotFoundException(Object id) {
+        super("Resource not found. Id " + id);
+    }
+}
