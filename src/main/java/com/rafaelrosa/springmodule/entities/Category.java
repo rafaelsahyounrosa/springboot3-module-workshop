86cbd63ddcad80a351363a5e9c557c4480c3fd72
Rafael Rosa <rafaelsahyoun@gmail.com>
2023-01-24 21:25:56 -0300

    ManytoMany association with jointable

--- src/main/java/com/rafaelrosa/springmodule/entities/Category.java
+++ src/main/java/com/rafaelrosa/springmodule/entities/Category.java
@@ -1,5 +1,6 @@
 package com.rafaelrosa.springmodule.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 
 import java.io.Serializable;
@@ -15,7 +16,9 @@ public class Category implements Serializable {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
-    @Transient
+
+    @JsonIgnore
+    @ManyToMany(mappedBy = "categories")
     private Set<Product> products = new HashSet<>();
 
     public Category() {
